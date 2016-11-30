3ec795dd88fa91c663b4eee3faada7242294407b
Justin Piper <jpiper4198@gmail.com>
2016-11-30 17:01:54 -0500

    basic filetree setup--could be changed up down the road

--- views.py
+++ views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 # Algorithm will go here
+
+def index(request):
+	# render home page
+	
+
+def file_check(request):
+	# check input file for FASTA format
+	
+
+def calculate(request):
+	# Algorithm goes here
