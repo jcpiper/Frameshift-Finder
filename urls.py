96adb1112fae1de9e94930a132578676ec4d37f7
Justin Piper <jpiper4198@gmail.com>
2016-12-01 14:11:18 -0500

    Can view pages now

---
+++ urls.py
@@ -0,0 +1,8 @@
+# urls
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+	url(r'^results', views.calculate, name='results'),
+]
--- views.py
+++ views.py
@@ -6,11 +6,24 @@ from django.shortcuts import render
 
 def index(request):
 	# render home page
-	
+	return render(request, 'fsfinder/index.html')
 
 def file_check(request):
 	# check input file for FASTA format
-	
+	# if correct format, pass sequence(s) onto calculate view
+	# if not, render an error page/allow user to submit another file
+	valid = True # set to false if any format errors are found
+	if valid:
+		calculate(request)
+	else:
+		context = {
+			'error': True,
+			# could add in a list of errors here
+		}
+		return render(request, 'fsfinder/index.html', context)
 
 def calculate(request):
 	# Algorithm goes here
+	
+	# Another file to build DFA?
+	return render(request, 'fsfinder/results.html')
