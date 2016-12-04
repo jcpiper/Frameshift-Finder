c6abbe7af8f0c97cbc3061798072b49fd5a61a00
Justin Piper <jpiper4198@gmail.com>
2016-12-04 14:17:53 -0500

    Will now loop through entire input file and read all orfs

--- FrameshiftFinder.py
+++ FrameshiftFinder.py
@@ -10,47 +10,56 @@ orfs = open('orfs.csv', 'w+')
 ## read entire sequence into a string
 sequence = dna.read()
 dna.close()
-sequence.upper()
+sequence = sequence.upper()
 
 ## find first start codon
-start = 0
-atg = sequence.find('ATG')
-if (atg >= 0):
-	start = atg
-gtg = sequence.find('GTG')
+while(len(sequence) > 3):
+	print('starting iteration')
+	print(len(sequence))
+	start = 0
+	atg = sequence.find('ATG')
+	if (atg >= 0):
+		start = atg
+	gtg = sequence.find('GTG')
 
-if (gtg >= 0 and gtg < start):
-	start = gtg
+	if (gtg >= 0 and gtg < start):
+		start = gtg
 
-# make sure start isn't mistakenly left as 0
-if (atg < 0 and gtg >= 0):
-	start = gtg
+	# make sure start isn't mistakenly left as 0
+	if (atg < 0 and gtg >= 0):
+		start = gtg
 
-ttg = sequence.find('TTG')
+	ttg = sequence.find('TTG')
 
-if (ttg >= 0 and ttg < start):
-	start = ttg
+	if (ttg >= 0 and ttg < start):
+		start = ttg
 
-# make sure start isn't mistakenly left as 0
-if(start == 0 and gtg < 0 and atg < 0 and ttg >= 0):
-	start = ttg
-	
-# Loop thru sequence until stop codon encountered
-curr = start
+	# make sure start isn't mistakenly left as 0
+	if(start == 0 and gtg < 0 and atg < 0 and ttg >= 0):
+		start = ttg
+	elif (gtg < 0 and atg < 0 and ttg < 0):
+		print('breaking out of loop?')
+		print('atg: ' + str(atg))
+		break #break out of loop if there are no more stop codons
+		
+	# Loop thru sequence until stop codon encountered
+	curr = start
 
-codon = sequence[curr:curr+3]
-rframe = []
-otherFrame = False
-
-while(codon != 'TGA' and codon !='TAA' and codon !='TAG'):
-	print(codon)
-	rframe.append(codon)
-	curr += 3
 	codon = sequence[curr:curr+3]
-	#check for another start codon
-	if (codon == 'ATG' or codon == 'GTG' or codon == 'TTG'):
-		otherFrame = True
-	
+	rframe = []
+	otherFrame = False
 
-print(rframe)
-orfs.close()
+	while(codon != 'TGA' and codon !='TAA' and codon !='TAG'):
+		print(codon)
+		rframe.append(codon)
+		codon = sequence[curr:curr+3]
+		sequence = sequence[curr+3:]
+		#check for another start codon
+		if (codon == 'ATG' or codon == 'GTG' or codon == 'TTG'):
+			otherFrame = True
+		
+
+	print(rframe)
+	print('remaining sequence: ' + sequence)
+	orfs.close()
+
