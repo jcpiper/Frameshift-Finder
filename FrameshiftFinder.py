498561e7dbbecdcc123ee01b7f2b34f37110e33c
Justin Piper <jpiper4198@gmail.com>
2016-12-04 13:46:06 -0500

    now finds correct start codon

--- FrameshiftFinder.py
+++ FrameshiftFinder.py
@@ -32,7 +32,7 @@ if (ttg >= 0 and ttg < start):
 	start = ttg
 
 # make sure start isn't mistakenly left as 0
-if(start == 0 and gtg < 0 and ttg >= 0):
+if(start == 0 and gtg < 0 and atg < 0 and ttg >= 0):
 	start = ttg
 	
 # Loop thru sequence until stop codon encountered
