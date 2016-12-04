06e3a73739505a4d880bbd9786d1f5544b3652fa
Justin Piper <jpiper4198@gmail.com>
2016-12-04 13:39:25 -0500

    Python script should run now

--- FrameshiftFinder.py
+++ FrameshiftFinder.py
@@ -1,13 +1,15 @@
 ## desktop version of algorithm
 
 ## Read in fasta file
-dna = open('test.fasta', 'r')
+dna = open('test.txt', 'r')
 
 ### File to be sent to glimmer ###
 orfs = open('orfs.csv', 'w+')
 
+
 ## read entire sequence into a string
 sequence = dna.read()
+dna.close()
 sequence.upper()
 
 ## find first start codon
@@ -36,18 +38,19 @@ if(start == 0 and gtg < 0 and ttg >= 0):
 # Loop thru sequence until stop codon encountered
 curr = start
 
-codon = sequence[curr:curr+2]
+codon = sequence[curr:curr+3]
 rframe = []
-otherFrame = false
+otherFrame = False
 
 while(codon != 'TGA' and codon !='TAA' and codon !='TAG'):
-	rframe.append(codon + ',')
+	print(codon)
+	rframe.append(codon)
 	curr += 3
-	codon = sequence[curr:curr+2]
+	codon = sequence[curr:curr+3]
 	#check for another start codon
-	if (codon == 'ATG' or codon == 'GTG' or codon == 'TTG')
-		otherFrame = true
+	if (codon == 'ATG' or codon == 'GTG' or codon == 'TTG'):
+		otherFrame = True
 	
 
-
-
+print(rframe)
+orfs.close()
